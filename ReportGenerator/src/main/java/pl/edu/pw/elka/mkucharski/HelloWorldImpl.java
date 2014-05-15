cdc59e25b034bd4c7aa9b42d9ee1b74709a93365
Maciej Kucharski <daark.lith@gmail.com>
2014-05-15 22:04:59 +0200

    Skonfigurowany pom, dorzucona prezentacja z SDI

--- ReportGenerator/src/main/java/pl/edu/pw/elka/mkucharski/HelloWorldImpl.java
+++ ReportGenerator/src/main/java/pl/edu/pw/elka/mkucharski/HelloWorldImpl.java
@@ -1,13 +1,20 @@
 
 package pl.edu.pw.elka.mkucharski;
 
+import pl.edu.pw.elka.mkucharski.groovy.GrvT;
+
 import javax.jws.WebService;
 
 @WebService(endpointInterface = "pl.edu.pw.elka.mkucharski.HelloWorld")
 public class HelloWorldImpl implements HelloWorld {
 
     public String sayHi(String text) {
-        return "Hello " + text;
+
+        GrvT g = new GrvT();
+        g.setAnother(25);
+        g.setSth("dsfs");
+
+        return g.getSth() + " " + g.getAnother();
     }
 }
 
